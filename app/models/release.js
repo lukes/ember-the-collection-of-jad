d14c561e74d534e5d363f1006a5a040211266a10
Luke Duncalfe <lduncalfe@eml.cc>
2015-12-15 17:01:05 +1300

    Adding more properties to models, from data

--- app/models/release.js
+++ app/models/release.js
@@ -5,8 +5,11 @@ export default DS.Model.extend({
   country: DS.attr(),
   format: DS.attr(),
   label: DS.attr(),
+  catno: DS.attr(),
   title: DS.attr(),
   year: DS.attr(),
+  manual: DS.attr(),
+  manual_reason: DS.attr(),
 
   genres: DS.hasMany('genre'),
   tracks: DS.hasMany('track'),
