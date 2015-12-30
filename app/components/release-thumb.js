6ba81386d380304754856870366df63d8ad9e17c
Luke Duncalfe <lduncalfe@eml.cc>
2015-12-31 11:56:05 +1300

    Basic styling for release thumb components.
    Adding microdata, basic responsive layout.

--- app/components/release-thumb.js
+++ app/components/release-thumb.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  classNames: 'release-thumb-wrapper pure-u-1-1 pure-u-md-1-2 pure-u-lg-1-3',
+
+  image: Ember.computed(function() {
+    return 'http://placehold.it/200x150'.htmlSafe()
+  })
+
 });
