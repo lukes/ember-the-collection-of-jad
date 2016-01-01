f73d1cd40a0038ea018196077af25240a3970c83
Luke Duncalfe <lduncalfe@eml.cc>
2016-01-01 19:46:09 +1300

    Track details component clickable, toggles playing of track

--- app/components/track-details.js
+++ app/components/track-details.js
@@ -1,13 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  audio: Ember.inject.service(),
+  classNames: 'track-details',
 
-  tagName: 'li',
+  audio: Ember.inject.service(),
 
   trackIsPlaying: Ember.computed('track', 'audio.track', 'audio.playing', function() {
     return this.get('track') === this.get('audio.track') && this.get('audio.playing') === true;
   }),
+
+  click: function() {
+    if (this.get('trackIsPlaying')) {
+      this.send('pause');
+    } else {
+      this.send('play');
+    }
+  },
 
   actions: {
 
