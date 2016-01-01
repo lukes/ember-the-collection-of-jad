6a75ce4bc8f738d508774ed79b15835e0c27a9d9
Luke Duncalfe <lduncalfe@eml.cc>
2016-01-02 07:19:37 +1300

    Simple state management for Audio Service

--- app/services/audio.js
+++ app/services/audio.js
@@ -6,41 +6,48 @@ export default Ember.Service.extend({
   howl: null,
   track: null,
   duration: null,
-  loading: false,
-  playing: false,
+  state: 'unloaded',
+  loading: Ember.computed.equal('state', 'loading'),
+  playing: Ember.computed.equal('state', 'playing'),
+  paused: Ember.computed.equal('state', 'paused'),
 
   play: function(track) {
     if (track && this.get('track') !== track) {
       this.reset();
-      this.set('loading', true);
+      this._setState('loading');
+
       let howl = new Howl({
         src: [track.get('audioUrl')]
       });
+
       howl.on('load', () => {
         Ember.run(() => {
           if (!this.get('isDestroyed')) {
             this.set('duration', howl.duration());
-            this.set('loading', false);
           }
         });
       });
+
       howl.on('pause', () => {
         Ember.run(() => {
           if (!this.get('isDestroyed')) {
-            this.set('playing', false);
+            this._setState('paused');
           }
         });
       });
+
       howl.on('play', () => {
         Ember.run(() => {
           if (!this.get('isDestroyed')) {
-            this.set('playing', true);
+            this._setState('playing');
           }
         });
       });
+
       this.set('howl', howl);
       this.set('track', track);
     }
+
     this.get('howl').play();
   },
 
@@ -55,8 +62,15 @@ export default Ember.Service.extend({
       this.get('howl').unload();
       this.set('howl', null);
       this.set('track', null);
+      this.set('state', 'unloaded');
       this.set('duration', null);
     }
+  },
+
+  _setState: function(state) {
+    let validStates = Ember.A(['unloaded', 'loading', 'playing', 'paused']);
+    Ember.assert(state + ' is a valid state', validStates.contains(state));
+    this.set('state', state);
   },
 
   tearDown: Ember.on('willDestroy', function() {
