0a5ff77dbff4227e1b96168ab485bf6a02f985f7
Luke Duncalfe <lduncalfe@eml.cc>
2016-01-01 19:47:03 +1300

    Release model splits tracks into A and B sides

--- app/models/release.js
+++ app/models/release.js
@@ -12,5 +12,18 @@ export default DS.Model.extend({
   manual_reason: DS.attr(),
 
   genres: DS.hasMany('genre'),
-  tracks: DS.hasMany('track')
+  tracks: DS.hasMany('track'),
+
+  aSideTracks: Ember.computed('tracks.@each.position', function() {
+    return this.get('tracks').filter((track) => {
+      return track.get('position').toLowerCase().substr(0, 1) == 'a';
+    });
+  }),
+
+  bSideTracks: Ember.computed('tracks.@each.position', 'aSideTracks.[]', function() {
+    return this.get('tracks').reject((track) => {
+      return this.get('aSideTracks').contains(track);
+    });
+  })
+
 });
