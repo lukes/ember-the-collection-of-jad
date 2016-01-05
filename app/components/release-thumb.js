3bdcd680e08e52b6879a433c6c82b3fbc9d2835d
Luke Duncalfe <lduncalfe@eml.cc>
2016-01-05 15:06:13 +1300

    More styling for release thumbs page

--- app/components/release-thumb.js
+++ app/components/release-thumb.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: 'release-thumb-wrapper pure-u-1-1 pure-u-md-1-2 pure-u-lg-1-3',
+  classNames: 'release-thumb-wrapper pure-u-md-1-1 pure-u-lg-1-2',
 
   image: Ember.computed(function() {
     return 'http://placehold.it/200x150'.htmlSafe();
--- app/components/release-thumbs.js
+++ app/components/release-thumbs.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: 'pure-g'
+  classNames: 'pure-g release-thumbs'
 });
